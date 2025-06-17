f9dbb41a0fce9d5c0318465427d157371febb62d
SrEloyF <fabrizioeloys@gmail.com>
2025-06-16 20:54:52 -0500

    fifth commit

---
+++ src/components/LargeComponent.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const LargeComponent = () => {
+  return (
+    <div>
+      <h2>Este es un componente grande</h2>
+      <p>Se carga dinámicamente solo cuando es necesario.</p>
+    </div>
+  );
+};
+
+export default LargeComponent;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,12 @@
-import Image from "next/image";
+import dynamic from "next/dynamic";
+
+const DynamicComponent = dynamic(() => import("../components/LargeComponent"), { ssr: false });
 
 export default function Home() {
   return (
     <>
       <h1>Bienvenido</h1>
-      <Image
-        src="/images/seo-image.png"
-        width={800}
-        height={400}
-        alt="Ejemplo de imagen optimizada"
-        priority
-      />
+      <DynamicComponent />
     </>
   );
 }
